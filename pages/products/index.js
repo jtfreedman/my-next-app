a99566660a2085d9f4be69968c767f6b38bb264d
jtfreedman <babzibles@gmail.com>
2022-05-18 00:06:53 -0400

    progress

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,12 +1,24 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { Fragment } from 'react'
 
 export default function Products() {
   return (
-    <div>
-      <Head>
-        <title>Products</title>
-      </Head>
-      
-    </div>
+    <Fragment>
+      <div>
+        <Head>
+          <title>Products</title>
+        </Head>
+        <h1>Products</h1>
+        <ul>
+          <li>
+            <a href='/products/item1'>Item 1</a>
+          </li>
+          <li>
+          <a href='/products/item2'>Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </Fragment>
   )
 }
